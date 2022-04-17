0d1b1f58a5fa927d4bea11a2e56442d5da1b0043
nhaatjlong <longdn555@gmail.com>
2022-04-17 13:34:44 +0700

    catche connect mongoDB

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = (handler) => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    // Use current db connection
+    return handler(req, res);
+  }
   mongoose.connect(process.env.NEXT_PUBLIC_DB_CONNECT, () =>
     console.log("Connected to DB")
   );
